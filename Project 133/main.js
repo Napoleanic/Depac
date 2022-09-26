7e29dac66e84e2688b46d51f48007b28010942e9
Napoleanic <90285645+Napoleanic@users.noreply.github.com>
2022-09-26 17:45:38 -0400

    Add files via upload

---
+++ Project 133/main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+    
+}
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+
+ellipse(56, 46, 55, 55);
+describe('white ellipse with black outline in middle of a gray canvas');
